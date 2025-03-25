78b046f34513ab24857361c6b4525fede47231b0
Clararaaa <clara01angelica@gmail.com>
2025-03-25 20:30:42 +0700

    Membuat TopAppBar Aplikasi

---
+++ app/src/main/java/com/clara0007/yummyfood/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.clara0007.yummyfood
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.clara0007.yummyfood.screen.MainScreen
+import com.clara0007.yummyfood.ui.theme.YummyFoodTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            YummyFoodTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                    MainScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    YummyFoodTheme {
+        Greeting("Android")
+    }
+}
