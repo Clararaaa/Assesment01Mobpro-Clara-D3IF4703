39dda716e1508b2a2155aac22d82d84c794bf431
Clararaaa <clara01angelica@gmail.com>
2025-04-09 22:15:34 +0700

    Membuat tampilan Cover

---
+++ app/src/main/java/com/clara0007/yummyfood/screen/CoverScreen.kt
@@ -0,0 +1,72 @@
+package com.clara0007.yummyfood.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.clara0007.yummyfood.R
+
+@Composable
+fun CoverScreen(onStartClick: () -> Unit) {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(24.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            // Nama aplikasi
+            Text(
+                text = stringResource(id = R.string.app_name),
+                style = MaterialTheme.typography.headlineMedium.copy(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 28.sp,
+                    color = MaterialTheme.colorScheme.primary
+                )
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            // Logo aplikasi
+            Image(
+                painter = painterResource(id = R.drawable.logo_app),
+                contentDescription = "Logo Aplikasi",
+                modifier = Modifier
+                    .size(180.dp)
+            )
+
+            Spacer(modifier = Modifier.height(36.dp))
+
+            // Tombol Pesan Sekarang
+            Button(
+                onClick = onStartClick,
+                colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary),
+                shape = MaterialTheme.shapes.medium
+            ) {
+                Text("Pesan Sekarang", color = Color.White)
+            }
+        }
+    }
+}
