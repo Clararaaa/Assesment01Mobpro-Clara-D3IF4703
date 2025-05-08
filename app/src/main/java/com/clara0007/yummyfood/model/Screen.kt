015d3f0e5624afb9a551cce3e95807823b5f10ea
Clararaaa <clara01angelica@gmail.com>
2025-05-08 21:14:02 +0700

    Membuat navigasi ke keranjang

---
+++ app/src/main/java/com/clara0007/yummyfood/model/Screen.kt
@@ -0,0 +1,6 @@
+package com.clara0007.yummyfood.model
+
+class Screen(val route: String) {
+    object Home : Screen("home")
+    object Keranjang : Screen("keranjang")
+}
--- app/src/main/java/com/clara0007/yummyfood/navigation/AppNavHost.kt
+++ app/src/main/java/com/clara0007/yummyfood/navigation/AppNavHost.kt
@@ -1,15 +1,22 @@
 package com.clara0007.yummyfood.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.clara0007.yummyfood.screen.CoverScreen
+import com.clara0007.yummyfood.screen.KeranjangScreen
 import com.clara0007.yummyfood.screen.MainScreen
+import com.clara0007.yummyfood.viewmodel.ViewDaftarMakanan
+import com.clara0007.yummyfood.model.Screen
 
 
 @Composable
 fun AppNavHost(navController: NavHostController) {
+
+    val viewModel: ViewDaftarMakanan = viewModel()
+
     NavHost(
         navController = navController,
         startDestination = "cover"
@@ -19,8 +26,16 @@ fun AppNavHost(navController: NavHostController) {
                 navController.navigate("main")
             })
         }
-        composable("main"){
-            MainScreen()
+        composable(Screen.Home.route) {
+            MainScreen(
+                viewModel = viewModel,
+                onKeranjangClick = {
+                    navController.navigate(Screen.Keranjang.route)
+                }
+            )
+        }
+        composable(Screen.Keranjang.route) {
+            KeranjangScreen(viewModel = viewModel)
         }
     }
 }
